e7274afba3589dcf3823b65598d6039385addd72
unknown <partharora557@gmail.com>
2023-03-27 10:45:40 +0530

    done

--- src/components/loader/not-available.jsx
+++ src/components/loader/not-available.jsx
@@ -4,12 +4,12 @@ const NotAvailable = () => {
 
   return (
     <div
-      className="w-[100vw] h-[100vh] bg-white fixed top-0 left-0 z-10 flex flex-col  items-center justify-center "
+      className="w-[100vw] p-[30px] h-[100vh] bg-white fixed top-0 left-0 z-10 flex flex-col  items-center justify-center "
       style={{
         zIndex: 100,
       }}
     >
-      <h1 className="text-black translate-y-[220px] font-medium tracking-wide ">
+      <h1 className="text-black translate-y-[200px] font-medium tracking-wide ">
         I'll be making the social media Demo Project here!
       </h1>
       <div className={classes.loader}>
